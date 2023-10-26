89aa0b6112ea3c42726931d73ff36309a5cdeb0c
Err-G <eriklustosa@gmail.com>
2023-10-26 18:19:19 +0100

    i do not know

--- get_next_line.c
+++ get_next_line.c
@@ -6,7 +6,7 @@
 /*   By: ecarvalh <ecarvalh@student.42porto.com>    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/10/15 12:44:45 by ecarvalh          #+#    #+#             */
-/*   Updated: 2023/10/22 22:30:36 by ecarvalh         ###   ########.fr       */
+/*   Updated: 2023/10/26 18:18:59 by ecarvalh         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -15,16 +15,26 @@
 char	*get_next_line(int fd)
 {
 	static char	buffer[BUFFER_SIZE + 1];
-	char		*temp;
+	char		*line;
 	ssize_t		len;
 
 	if (fd < 0 || BUFFER_SIZE <= 0)
 		return (NULL);
-	len = read(fd, buffer, BUFFER_SIZE);
-	while (!ft_strchr(buffer, '\n'));
+	line = (char *)malloc((BUFFER_SIZE + 1) * sizeof(char));
+	if (!line)
+		return (NULL);
+	len = 1;
+	while (!(ft_strchr(buffer, '\n')) && len != 0)
 	{
-		buffer[len + 1] = 0;
-		temp = ft_strjoin(temp, buffer);
+		len = read(fd, buffer, BUFFER_SIZE);
+		if (len == -1)
+		{
+			free(tmp);
+			return (NULL);
+		}
+		line[len] = '\0';
+		line = ft_strjoin(line, buffer);
 	}
-	return (tmp);
+	line = ft_strjoin(line, buffer);
+	return (line);
 }
